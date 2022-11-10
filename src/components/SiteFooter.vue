387520074d277796084b48e3b2fe7f4527ea000b
macchiagabriele13 <macchia_gabriele@libero.it>
2022-11-10 16:32:23 +0100

    Commit - Inizio Footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -7,6 +7,91 @@ export default {
 <template>
   <footer>
 
+    <div class="section_footer_menu py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="footer_list">
+              <h2>Dc comics</h2>
+              <ul>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+              </ul>
+
+            </div>
+            <div class="footer_list">
+              <h2>Shops</h2>
+              <ul>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+
+              </ul>
+
+            </div>
+          </div>
+          <div class="col">
+            <div class="footer_list">
+              <h2>Dc</h2>
+              <ul>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+
+              </ul>
+
+            </div>
+
+          </div>
+          <div class="col">
+            <div class="footer_list">
+              <h2>Sites</h2>
+              <ul>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+                <li><a href=""></a>Prova</li>
+
+
+              </ul>
+
+            </div>
+          </div>
+          <div class="col">
+            <div class="footer_image">
+              <img src="../assets/img/dc-logo-bg.png" alt="">
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="social">
+      <div class="container d-flex justify-content-between align-center">
+        <button>Sign Up</button>
+
+        <div>
+          <h2>FOLLOW US</h2>
+          <a href="#"><i class="fas fa-facebook"></i></a>
+        </div>
+
+      </div>
+
+    </div>
   </footer>
 
 
