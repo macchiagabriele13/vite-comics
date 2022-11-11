f40bfabbe6fcd5d761639be98b04ed477bf02ee6
macchiagabriele13 <macchia_gabriele@libero.it>
2022-11-11 14:30:53 +0100

    Commit- Grid Responsive JS

---
+++ src/components/Comic.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    props: {
+        image: String,
+        category: String,
+    }
+}
+</script>
+
+<template>
+
+
+    <div class="col-2">
+        <img :src="image" alt="">
+        <p>{{ category }}</p>
+    </div>
+
+
+
+</template>
+
+<style>
+
+</style>
--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -1,10 +1,14 @@
 <script>
 import dc_comics from '../data/dc_comics.js'
+import Comic from '../components/Comic.vue'
 export default {
     name: 'Comics',
+    components: {
+        Comic,
+    },
     data() {
         return {
-            fumetti: dc_comics
+            comics: dc_comics
         }
     }
 }
@@ -16,12 +20,12 @@ export default {
         <div class="container">
             <h3 class="badge">Current Series</h3>
 
-            <div class="row">
-                <div class="col">
-                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-                        alt="">
-                    <p>Titolo</p>
-                </div>
+            <div class="row ">
+                <Comic v-for="comic in comics" :image='comic.thumb' :category='comic.series' />
+                <!-- <div class="col-2" v-for="comic in comics">
+                    <img :src="comic.thumb" alt="">
+                    <p>{{ comic.series }}</p>
+                </div> -->
             </div>
 
         </div>
